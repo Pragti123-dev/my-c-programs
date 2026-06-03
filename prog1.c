65c029a0039d38429753753110dc314534f5a136
PRAGATI PAL <pragtipal853@gmail.com>
2026-06-04 01:54:16 +0530

    Add files via upload

---
+++ prog1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    
+    printf("Enter a number to print its multiplication table: ");
+    scanf("%d", &num);
+
+    printf("\nMultiplication Table for %d:\n", num);
+    printf("-----------------------------\n");
+
+    
+    for (int i = 1; i <= 10; i++) {
+        printf("%d x %d = %d\n", num, i, num * i);
+    }
+
+    return 0;
+}
