2ab8674c4b44c8a0d3b86aed4515d1cd381d1fff
PRAGATI PAL <pragtipal853@gmail.com>
2026-06-09 12:37:32 +0530

    Add files via upload

---
+++ prog32.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int i, j, rows = 5;
+
+    // Outer loop: Controls the number of rows
+    for(i = 1; i <= rows; i++) {
+        
+        
+        for(j = 1; j <= i; j++) {
+            printf("%d", i); 
+        }
+        
+        printf("\n"); 
+    }
+
+    return 0;
+}
