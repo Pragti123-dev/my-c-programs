60332ebb4f64f540499409f4c05cc25d890f6475
PRAGATI PAL <pragtipal853@gmail.com>
2026-06-13 21:55:33 +0530

    Add files via upload

---
+++ prog41.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+int findSum(int num1, int num2);
+
+int main() {
+    int a, b, sum;
+
+    
+    printf("Enter the first number: ");
+    scanf("%d", &a);
+
+    printf("Enter the second number: ");
+    scanf("%d", &b);
+
+    
+    sum = findSum(a, b);
+
+    
+    printf("The sum of %d and %d is: %d\n", a, b, sum);
+
+    return 0;
+}
+
+
+int findSum(int num1, int num2) {
+    return num1 + num2;
+}
